0abba843fd50f1fe06c9787830f319e9e58c186b
vanyan12 <84772245+vanyan12@users.noreply.github.com>
2023-11-05 11:23:21 +0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
     console.log(TaskImgs)
 
        TaskImgs.forEach((image) => {
-        image.scr = `https://github.com/vanyan12/Math_Test_Generator/blob/pdf/Pictures/2Section1/2Section1-1.png?raw=true`
+        image.scr = `../Pictures/$(image.id)`
        })
 
 })
