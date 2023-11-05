66490c8a98db9a145169c2de543289d08ac50896
vanyan12 <84772245+vanyan12@users.noreply.github.com>
2023-11-05 11:32:50 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 $(document).ready(() => {
     let TaskImgs = document.querySelectorAll('img')
 
-    console.log('js')
-    console.log(TaskImgs)
 
        TaskImgs.forEach((image) => {
-        image.scr = `../Pictures/$(image.id)/$(image.id)-Math.round(Math.random() * 10).png`
+        image.scr = `./Pictures/$(image.id)/$(image.id)-$(Math.round(Math.random() * 10)).png`
+           console.log(image.src)
        })
 
 })
