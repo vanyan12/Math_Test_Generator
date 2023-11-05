bbb99440dc1a19cb07390c54b49daa21cb4c7b79
vanyan12 <84772245+vanyan12@users.noreply.github.com>
2023-11-05 11:07:21 +0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
     let TaskImgs = document.querySelectorAll('img')
 
     console.log('js')
+    console.log(TaskImgs)
 
        TaskImgs.forEach((image) => {
         image.scr = `https://github.com/vanyan12/Math_Test_Generator/blob/pdf/Pictures/2Section1/2Section1-1.png?raw=true`
