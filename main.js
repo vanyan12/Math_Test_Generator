82afe58d125943b1ac4779eae439fa8d08d0a335
vanyan12 <84772245+vanyan12@users.noreply.github.com>
2023-12-17 16:17:48 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
 $(document).ready(() => {
     let TaskImgs = document.querySelectorAll('img')
+    let SecCount = {
+        'Section1': 37,
+        'Section2': 73,
+        'Section3': 63,
+        'Section4': 49,
+        'Section5': 66,
+        'Section6': 23,
+        'Section7': 20,
+        'Section8': 68,
+        'Section9': 23,
+        'Section10': 26,
+        '2Section1': 43,
+        '2Section2': 19,
+        '2Section3': 19,
+        '2Section4': 23,
+        '2Section5': 35,
+        '2Section6': 43,
+        '2Section7': 29,
+        '2Section8': 86,
+        '3Section1': 46,
+        '3Section2': 77,
+        '3Section4': 16   
+    }
 
 
        TaskImgs.forEach((image) => {
-        image.scr = `./Pictures/$(image.id)/$(image.id)-Math.round(Math.random() * 10).png`
-           console.log(image.src)
+        image.src = `Pictures/${image.id}/${image.id}-${(Math.ceil(Math.random() * SecCount[image.id]))}.png`
        })
 
 })
